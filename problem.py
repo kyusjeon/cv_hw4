5ec43d5e4a1b85a73b2a74c4a2e70554e4143231
전규성 <tjsanf0606@mgail.com>
2022-05-16 17:39:48 +0900

    set get feature point

--- problem.py
+++ problem.py
@@ -1,17 +1,97 @@
+import os
 import cv2 
 import time
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-def load_image(path):
-    pass
+def filter(weights,roi):
+    weights = weights.astype(float)   
+    roi = roi.astype(float)   
+    filtered = np.zeros_like(roi)
+    width = int((weights.shape[1]-1)/2)
+    height = int((weights.shape[0]-1)/2)
+    
+    for i in range(height,roi.shape[1]-height):
+        for j in range(width,roi.shape[0]-width):
+            filtered[j,i] = (weights * roi[j-width:j+width+1, i-height:i+height+1]).sum()       # how do you create the output of the filtering?
+    
+    return  filtered
 
-def get_feature_point(img):
-    # returen feature_points
-    pass
+def gaussian_kernel(l):
+    sig = int((l-1)/2)
+    ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
+    gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
+    kernel = np.outer(gauss, gauss)
+    return kernel / np.sum(kernel)
 
-def non_maximum_suppression(feature_map):
+
+def harris_oper(a,b,c):
+    oper = np.nan_to_num((a * c - b * b) / (a + c))
+
+    return oper
+
+def corner_detect(img, win_size=7):
+    filter_sobelx = np.array([
+    [-1,0,1],
+    [-2,0,2],
+    [-1,0,1],
+    ])
+
+    filter_sobely = np.array([
+    [1,2,1],
+    [0,0,0],
+    [-1,-2,-1],
+    ])
+
+    filtered_x = filter(filter_sobelx, img)
+    filtered_y = filter(filter_sobely, img)
+
+    window = gaussian_kernel(win_size)
+
+    harris_a = filter(window, filtered_x*filtered_x)
+    harris_b = filter(window, filtered_x*filtered_y)
+    harris_c = filter(window, filtered_y*filtered_y)
+
+    harris_h = harris_oper(harris_a, harris_b, harris_c)
+
+    return harris_h
+
+def load_image(root_path, image_path_list):
+    image_list = list()
+    for _i in image_path_list:
+        _img = cv2.imread(os.path.join(root_path, _i))
+        _img = cv2.cvtColor(_img, cv2.COLOR_BGR2GRAY)
+        _img = cv2.resize(_img, dsize=(800,1000))
+        image_list.append(_img)
+    
+    return image_list
+
+def non_maximum_suppression(feature_map, win_size=7):
+    for _i in range(0,feature_map.shape[1]-win_size):
+        for _j in range(0,feature_map.shape[0]-win_size):
+            feature_map[_j:_j + win_size, _i:_i + win_size] = np.where(feature_map[_j:_j + win_size, _i:_i + win_size] == np.max(feature_map[_j:_j + win_size, _i:_i + win_size]), 
+                                                                       feature_map[_j:_j + win_size, _i:_i + win_size], 
+                                                                       0)
+    
+    return feature_map
+
+def get_feature_point(img, win_size=7, thr=0.3):
+    feature_map = corner_detect(img, win_size=win_size)
+    feature_map = np.where(feature_map > feature_map.max()*thr, feature_map, 0)
+    feature_map = non_maximum_suppression(feature_map, win_size=win_size)
+    
+    return np.argwhere(feature_map != 0), feature_map
+
+root_path = './image_folder'
+image_path_list = sorted(os.listdir(root_path))
+image_path_sample = list()
+for _i, _j in enumerate(image_path_list):
+    if _i % 8 == 0:
+        image_path_sample.append(_j)
+
+image_list = load_image(root_path, image_path_sample)
+for _i in image_list:
     pass
 
 def get_feature_descriptors(feature_points, window_size):
